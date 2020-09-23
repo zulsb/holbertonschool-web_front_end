40a58d306df6e534aa19c02be6da18f5a5fa8db0
zulsb <zulsb2093@gmail.com>
2020-09-22 23:06:03 -0500

    Prime numbers & timing execution

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+    let limit = 101;
+    let n = [...Array(limit).keys()];
+
+    for (let i = 2; i * i < limit; i++) {
+        if (n[i] !== "1") {
+            for (let j = i * i; j < limit; j += i) {
+                n[j] = "1";
+            }
+        }
+    }
+
+    let primes = [];
+    for (let i = 0; i < n.length; i++) {
+        if (n[i] > 1) {
+            primes.push(n[i]);
+        }
+    }
+
+    return primes.length;
+};
+
+let ts = performance.now();
+countPrimeNumbers();
+let tf = performance.now();
+console.log("Execution time of printing countPrimeNumbers was " + (tf - ts) + " milliseconds.");
